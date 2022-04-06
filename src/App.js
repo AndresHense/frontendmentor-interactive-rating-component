2fd5cbe581197f50a827847b5d656bc3b90e5aeb
AndresHense <andreshense@gmail.com>
2022-04-06 16:49:36 -0300

    add responsiveness

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import './App.css';
 import {
   Stack,
-  Box,
   HStack,
   Button,
   Text,
-  useRadio,
   useRadioGroup,
-  Circle,
   Center,
-  Icon,
+  Circle,
 } from '@chakra-ui/react';
 import { useState } from 'react';
-import SubmitedSVG from './SubmitedSVG';
-
-function RadioCard(props) {
-  const { getInputProps, getCheckboxProps } = useRadio(props);
-
-  const input = getInputProps();
-  const checkbox = getCheckboxProps();
-
-  return (
-    <Box as='label'>
-      <input {...input} />
-      <Circle
-        {...checkbox}
-        cursor='pointer'
-        bg='#262f38'
-        boxShadow='md'
-        _checked={{
-          bg: '#fc7613',
-          color: 'white',
-          borderColor: '#fc7613',
-        }}
-        _hover={{
-          bg: '#7c8799',
-        }}
-        px={6}
-        py={4}
-        mx={2}
-        my={2}
-      >
-        {props.children}
-      </Circle>
-    </Box>
-  );
-}
+import SubmitedSVG from './svgs/SubmitedSVG';
+import RadioCard from './components/RadioCard';
+import StarSVG from './svgs/StarSVG';
 
 function App() {
   const options = ['1', '2', '3', '4', '5'];
-  const [showModal, setShowModal] = useState(false);
+  const [showThanks, setShowThanks] = useState(false);
 
   const [rating, setRating] = useState('1');
 
@@ -63,39 +29,30 @@ function App() {
   const group = getRootProps();
 
   const handleSubmit = () => {
-    setShowModal(true);
+    setShowThanks(true);
   };
 
   const handleReturn = () => {
-    setShowModal(false);
+    setShowThanks(false);
   };
+
   return (
     <Center bg='#141519' height='100vh'>
       <Stack
         color='white'
         bg='#1f2630'
-        width='27%'
-        height='50%'
-        m='1rem'
-        my='1.5rem'
+        width={{ base: '90%', md: '65%', lg: '27%' }}
+        height={{ base: '80%', md: '65%', lg: '50%' }}
+        m='2rem'
         p='3rem'
+        py='4rem'
         borderRadius='2rem'
       >
-        {!showModal && (
+        {!showThanks && (
           <>
-            <Icon
-              viewBox='0 0 20 20'
-              boxSize={8}
-              bg='#262d37'
-              borderRadius='lg'
-              my='1rem'
-              p='3px'
-            >
-              <path
-                fill='#FC7614'
-                d='m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z'
-              />
-            </Icon>
+            <Circle bg='#262f38' my='0.4rem' width='40px' height='40px'>
+              <StarSVG />
+            </Circle>
             <Text fontSize='4xl' fontWeight='bold' as='h1'>
               How did we do?
             </Text>
@@ -126,7 +83,7 @@ function App() {
             </Button>
           </>
         )}
-        {showModal && (
+        {showThanks && (
           <>
             <Center>
               <Stack>
---
+++ src/components/RadioCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useRadio, Box, Circle } from '@chakra-ui/react';
+const RadioCard = (props) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as='label'>
+      <input {...input} />
+      <Circle
+        {...checkbox}
+        cursor='pointer'
+        bg='#262f38'
+        boxShadow='md'
+        _checked={{
+          bg: '#fc7613',
+          color: 'white',
+          borderColor: '#fc7613',
+        }}
+        _hover={{
+          bg: '#7c8799',
+        }}
+        px={{ base: 5, md: 6, lg: 6 }}
+        py={{ base: 3, md: 4, lg: 4 }}
+        mx={{ base: 2, md: 2, lg: 2 }}
+        my={{ base: 2, md: 4, lg: 4 }}
+      >
+        {props.children}
+      </Circle>
+    </Box>
+  );
+};
+
+export default RadioCard;
